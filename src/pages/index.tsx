2e0d105f5bcffb3116d9c37fce138d87e206cac5
s1f102201391 <107389125+s1f102201391@users.noreply.github.com>
2024-06-13 01:30:17 +0900

    レベル1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
   const clickHandler = (x: number, y: number) => {
+    if (board[y][x] !== 0) return;
     console.log(x, y);
     const newBoard = structuredClone(board);
     const directions = [
@@ -30,19 +31,52 @@ const Home = () => {
       const [a, b] = item;
       let X = x + a;
       let Y = y + b;
-      while (board[Y] !== undefined && board[Y][X] !== undefined)
-        if (board[Y][X] === 2 / turnColor) {
-          Y += b;
-          X += a;
-        } else if (board[Y][X] === turnColor) {
-          newBoard[y][x] = turnColor;
-          setTurnColor(2 / turnColor);
-          break;
+      let n = 0;
+      if (board[Y] !== undefined && board[Y][X] !== undefined && board[Y][X] === 2 / turnColor) {
+        while (n >= 0) {
+          if (
+            board[Y] !== undefined &&
+            board[Y][X] !== undefined &&
+            board[Y][X] === 2 / turnColor
+          ) {
+            Y += b;
+            X += a;
+            n += 1;
+          } else if (
+            board[Y] !== undefined &&
+            board[Y][X] !== undefined &&
+            board[Y][X] === turnColor
+          ) {
+            while (n > 0) {
+              Y -= b;
+              X -= a;
+              n -= 1;
+              newBoard[Y][X] = turnColor;
+            }
+            newBoard[y][x] = turnColor;
+            setTurnColor(2 / turnColor);
+            break;
+          } else {
+            break;
+          }
         }
+      }
     }
-
     setBoard(newBoard);
   };
+  const countStone = () => {
+    let Black = 0;
+    let White = 0;
+    for (let a = 0; a < 8; a++) {
+      for (let b = 0; b < 8; b++) {
+        if (board[a][b] === 1) Black++;
+        if (board[a][b] === 2) White++;
+      }
+    }
+    return { Black, White };
+  };
+  const result = countStone();
+
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
@@ -58,6 +92,13 @@ const Home = () => {
             </div>
           )),
         )}
+      </div>
+
+      <div className={styles.scoreBoard}>
+        <p>
+          黒{result.Black}/白{result.White}
+        </p>
+        <p>{turnColor === 1 ? '黒' : '白'}のターン</p>
       </div>
     </div>
   );
