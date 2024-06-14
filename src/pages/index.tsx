976eeeb39b29648b1771699a26f173b131461c5e
s1f102201391 <107389125+s1f102201391@users.noreply.github.com>
2024-06-14 13:02:33 +0900

    レベル2完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,34 +6,73 @@ const Home = () => {
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 0, 0, 0],
-    [0, 0, 0, 2, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const directions = [
+    [0, 1], //下
+    [0, -1], //上
+    [1, 0], //右
+    [-1, 0], //左
+    [1, 1], //右下
+    [1, -1], //右上
+    [-1, 1], //左下
+    [-1, -1], //左上
+  ];
   //候補地
+  const newBoard = structuredClone(board);
+
   const nextClick = (nowTurncolor: number) => {
-    for (let a = 0; a < 8; a++) {
-      for (let b = 0; b < 8; b++) {}
+    for (let d = 0; d < 8; d++) {
+      for (let c = 0; c < 8; c++) {
+        if (newBoard[c][d] === 3) {
+          newBoard[c][d] = 0;
+        }
+        for (const item of directions) {
+          const [a, b] = item;
+          let X = d + a;
+          let Y = c + b;
+          if (newBoard[c][d] === 0) {
+            if (
+              newBoard[Y] !== undefined &&
+              newBoard[Y][X] !== undefined &&
+              newBoard[Y][X] === 2 / nowTurncolor
+            ) {
+              while (newBoard[Y] !== undefined && newBoard[Y][X] !== undefined) {
+                if (
+                  newBoard[Y] !== undefined &&
+                  newBoard[Y][X] !== undefined &&
+                  newBoard[Y][X] === 2 / nowTurncolor
+                ) {
+                  Y += b;
+                  X += a;
+                } else if (
+                  newBoard[Y] !== undefined &&
+                  newBoard[Y][X] !== undefined &&
+                  newBoard[Y][X] === nowTurncolor
+                ) {
+                  if (newBoard[c][d] !== 0) return;
+                  newBoard[c][d] = 3;
+                  break;
+                } else {
+                  break;
+                }
+              }
+            }
+          }
+        }
+      }
     }
   };
 
   const clickHandler = (x: number, y: number) => {
-    if (board[y][x] !== 0) return;
+    if (board[y][x] !== 3) return;
     console.log(x, y);
-    const newBoard = structuredClone(board);
-    const directions = [
-      [0, 1], //下
-      [0, -1], //上
-      [1, 0], //右
-      [-1, 0], //左
-      [1, 1], //右下
-      [1, -1], //右上
-      [-1, 1], //左下
-      [-1, -1], //左上
-    ];
+
     //8方向
     for (const item of directions) {
       const [a, b] = item;
@@ -70,6 +109,8 @@ const Home = () => {
         }
       }
     }
+    nextClick(2 / turnColor);
+    setTurnColor(2 / turnColor);
     setBoard(newBoard);
   };
   const countStone = () => {
@@ -93,17 +134,14 @@ const Home = () => {
             <div className={styles.cellStyle} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
               {color !== 0 && (
                 <div
-                  className={styles.stoneStyle}
-                  style={{ background: color === 1 ? '#000' : '#fff' }}
-                >
-                  <div className={styles.nextPoint} />
-                </div>
+                  className={`${styles.stoneStyle} ${color === 3 ? styles.nextPoint : ''}`}
+                  style={{ background: color === 1 ? '#000' : color === 2 ? '#FFF' : '#FF0' }}
+                />
               )}
             </div>
           )),
         )}
       </div>
-
       <div className={styles.scoreBoard}>
         <p>
           黒{result.Black}/白{result.White}
@@ -115,3 +153,20 @@ const Home = () => {
 };
 
 export default Home;
+
+// for (let n = 0; n < 8; n++) {
+//   if (
+//     board[y] !== undefined &&
+//     board[y + directions[n][0]][x + directions[n][1]] !== undefined &&
+//     board[y + directions[n][0]][x + directions[n][1]] === 2 / turnColor
+//   ) {
+//     if (board[y + directions[n][0]][x + directions[n][1]] === turnColor) {
+//       for (let m = n; m >= 0; m--) {
+//         board[y + directions[n * m][0]][x + directions[n * m][1]] === turnColor;
+//       }
+//       newBoard[y][x] = turnColor;
+//       setTurnColor(2 / turnColor);
+//       break;
+//     }
+//   }
+// }
